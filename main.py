da4ec8ac9ff2eddd1d6462522696c8c46924293b
silvertakana <titmitna@gmail.com>
2025-07-09 17:46:01 +1200

    Enhance main.py with player movement, camera control, and scene creation. Update requirements to include pyglet. Adjust tiled session file for project path organization and view center coordinates.

--- main.py
+++ main.py
@@ -1,97 +1,205 @@
 import arcade
+import random
+from pyglet.math import Vec2
 
+# Window settings
+WINDOW_TITLE = "Starting Template"
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
-WINDOW_TITLE = "Starting Template"
+
+# Constants
+CHARACTER_SCALING = 1
+TILE_SCALING = 0.5
+COLLECTABLE_SCALING = 0.5
+SPRITE_PIXEL_SIZE = 128
+GRID_PIXEL_SIZE = SPRITE_PIXEL_SIZE * TILE_SCALING
+
+# Player Movement
+PLAYER_MOVEMENT_SPEED = 10
+
+# Camera constants
+FOLLOW_DECAY_CONST = 0.3  # get within 1% of the target position within 2 seconds
+
+VIEWPORT_MARGIN = 250
+HORIZONTAL_BOUNDARY = WINDOW_WIDTH / 2.0 - VIEWPORT_MARGIN
+VERTICAL_BOUNDARY = WINDOW_HEIGHT / 2.0 - VIEWPORT_MARGIN
+
+# Key constants
+LEFT_KEY = arcade.key.LEFT
+RIGHT_KEY = arcade.key.RIGHT
+UP_KEY = arcade.key.UP
+DOWN_KEY = arcade.key.DOWN
+
+def to_vector(point: tuple[float, float] | arcade.types.Point2) -> Vec2:
+    return Vec2(point[0], point[1])
+
+
+
+class Entity(arcade.Sprite):
+    def __init__(self, image_path, scale=1.0):
+        super().__init__(image_path, scale=scale)
+        self.position = (0, 0)
+        self.velocity = (0, 0)
 
 
 class GameView(arcade.View):
     """
     Main application class.
-
-    NOTE: Go ahead and delete the methods you don't need.
-    If you do need a method, delete the 'pass' and replace it
-    with your own code. Don't leave 'pass' in this program.
     """
 
     def __init__(self):
         super().__init__()
 
-        self.background_color = arcade.color.AMAZON
+        self.window.background_color = arcade.color.AMAZON
 
-        # If you have sprite lists, you should create them here,
-        # and set them to None
+        # Camera for scrolling
+        self.camera = arcade.Camera2D()
+        self.camera_bounds = self.window.rect
+        
+        # A non-scrolling camera that can be used to draw GUI elements
+        self.camera_gui = arcade.Camera2D()
+
+        self.scene = self.create_scene()
+
+        # Set up the player info
+        self.player = Entity(
+            ":resources:images/animated_characters/female_adventurer/femaleAdventurer_idle.png",
+            scale=CHARACTER_SCALING,
+        )
+
+        self.physics_engine = arcade.PhysicsEngineSimple(
+            self.player,
+            self.scene.get_sprite_list("Platforms"),
+        )
+
+        # Track the current state of what key is pressed
+        self.key_down = {
+            LEFT_KEY: False,
+            RIGHT_KEY: False,
+            UP_KEY: False,
+            DOWN_KEY: False,
+        }
+
+        # List of debug text objects
+        self.debug_dict = {}
+
+
+        self.reset()
+
+        
+    def create_scene(self) -> arcade.Scene:
+        """ Set up the game and initialize the variables. """
+        scene = arcade.Scene()
+        spacing = 200
+        for column in range(10):
+            for row in range(10):
+                sprite = arcade.Sprite(
+                    ":resources:images/tiles/grassCenter.png",
+                    scale=TILE_SCALING,
+                )
+
+                x = (column + 1) * spacing
+                y = (row + 1) * sprite.height
+
+                sprite.center_x = x
+                sprite.center_y = y
+                if random.randrange(100) > 20:
+                    scene.add_sprite("Platforms", sprite)
+
+        return scene
+
 
     def reset(self):
-        """Reset the game to the initial state."""
-        # Do changes needed to restart the game here if you want to support that
-        pass
+        self.scene = self.create_scene()
+
+        self.player.position = (50, 350)
+        self.scene.add_sprite("Player", self.player) 
+        self.physics_engine = arcade.PhysicsEngineSimple(
+            self.player,
+            self.scene.get_sprite_list("Platforms"),
+        )
 
     def on_draw(self):
-        """
-        Render the screen.
-        """
+        """ Render the screen. """
 
-        # This command should happen before we start drawing. It will clear
-        # the screen to the background color, and erase what we drew last frame.
         self.clear()
 
-        # Call draw() on all your sprite lists below
+        with self.camera.activate():
+            self.scene.draw()
 
+        with self.camera_gui.activate():
+            debug_text = arcade.Text(
+                "",
+                x=10,
+                y=10,
+                color=arcade.csscolor.WHITE,
+                font_size=18,
+            )
+            for key, text in self.debug_dict.items():
+                debug_text.text += f"{key}: {text}\n"
+            debug_text.draw()
+
+    def update_player_speed(self):
+        # Calculate speed based on the keys pressed
+        self.player.change_x = 0
+
+        if self.key_down[LEFT_KEY] and not self.key_down[RIGHT_KEY]:
+            self.player.change_x = -PLAYER_MOVEMENT_SPEED
+        elif self.key_down[RIGHT_KEY] and not self.key_down[LEFT_KEY]:
+            self.player.change_x = PLAYER_MOVEMENT_SPEED
+
+        self.player.change_y = 0
+
+        if self.key_down[UP_KEY] and not self.key_down[DOWN_KEY]:
+            self.player.change_y = PLAYER_MOVEMENT_SPEED
+        elif self.key_down[DOWN_KEY] and not self.key_down[UP_KEY]:
+            self.player.change_y = -PLAYER_MOVEMENT_SPEED
+
+        # Normalize the speed
+        self.player.velocity = to_vector(self.player.velocity).normalize() * PLAYER_MOVEMENT_SPEED
+        self.add_debug_text("Velocity", f"{self.player.velocity.x}, {self.player.velocity.y}")
+
+    def on_key_press(self, key, modifiers):
+        self.key_down[key] = True
+
+    def on_key_release(self, key, modifiers):
+        self.key_down[key] = False
+    def center_camera_to_player(self):
+        # Move the camera to center on the player
+        self.camera.position = arcade.math.smerp_2d(
+            self.camera.position,
+            self.player.position,
+            self.window.delta_time,
+            FOLLOW_DECAY_CONST,
+        )
+
+        # Constrain the camera's position to the camera bounds.
+        self.camera.view_data.position = arcade.camera.grips.constrain_xy(
+            self.camera.view_data, self.camera_bounds
+        )    
+        
     def on_update(self, delta_time):
-        """
-        All the logic to move, and the game logic goes here.
-        Normally, you'll call update() on the sprite lists that
-        need it.
-        """
-        pass
+        self.update_player_speed()
+        self.physics_engine.update()
+        self.center_camera_to_player()  
+    def on_resize(self, width: int, height: int):
+        """ Resize window """
+        super().on_resize(width, height)
+        # Update the cameras to match the new window size
+        self.camera.match_window()
+        # The position argument keeps `0, 0` in the bottom left corner.
+        self.camera.match_window(position=True)
 
-    def on_key_press(self, key, key_modifiers):
-        """
-        Called whenever a key on the keyboard is pressed.
-
-        For a full list of keys, see:
-        https://api.arcade.academy/en/latest/arcade.key.html
-        """
-        pass
-
-    def on_key_release(self, key, key_modifiers):
-        """
-        Called whenever the user lets off a previously pressed key.
-        """
-        pass
-
-    def on_mouse_motion(self, x, y, delta_x, delta_y):
-        """
-        Called whenever the mouse moves.
-        """
-        pass
-
-    def on_mouse_press(self, x, y, button, key_modifiers):
-        """
-        Called when the user presses a mouse button.
-        """
-        pass
-
-    def on_mouse_release(self, x, y, button, key_modifiers):
-        """
-        Called when a user releases a mouse button.
-        """
-        pass
+    def add_debug_text(self, key: str, text: str):
+        self.debug_dict[key] = text
 
 
 def main():
     """ Main function """
-    # Create a window class. This is what actually shows up on screen
     window = arcade.Window(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE)
-
-    # Create and setup the GameView
     game = GameView()
 
-    # Show GameView on screen
     window.show_view(game)
-
-    # Start the arcade game loop
     arcade.run()
 
 
